b13936709107be4e398b160cdf11a4665eea4188
httpheroku <htttpheroku@gmail.com>
2024-09-18 18:04:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "",
+SESSION_ID: process.env.SESSION_ID || "JXsTBbhQ#-J-YJdgV5iuWrDL31GcYZdPpayNpcsvtwYo-wApVG2E",
+MONGODB: process.env.MONGODB || "mongodb+srv://KINGANJANA:123455@cluster0.t0jie.mongodb.net/",
 };
